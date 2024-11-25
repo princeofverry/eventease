4562a4953d53f76653d332472bbc7fead8898e7c
princeofverry <verrykurniawan@students.undip.ac.id>
2024-11-25 17:26:21 +0700

    feat: aos

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
+"use client";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import HomePage from "@/app/Pages/HomePage";
 import ScrollableCards from "@/components/Cards/ScrollableCards";
 import Testimonial from "@/components/Cards/Testimonial";
@@ -7,31 +11,40 @@ import Contact from "@/components/ui/Contact";
 import { cardData } from "@/data/cardData";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Durasi dari aosnya
+      once: true, // Cuma bisa sekali aja
+    });
+  }, []);
+
   return (
     <>
       <HomePage />
-      <CardDaftar />
+      <div data-aos="fade-up">
+        <CardDaftar />
+      </div>
       {/* <EventOrganizer /> */}
-      <section>
+      <section data-aos="fade-up">
         <h2 className="text-2xl font-bold px-12">Event Organizer</h2>
         <ScrollableCards type="eo" data={cardData.eo} />
       </section>
 
-      <section>
+      <section data-aos="fade-up">
         <h2 className="text-2xl font-bold px-12">Vendor</h2>
         <ScrollableCards type="vendor" data={cardData.vendor} />
       </section>
 
-      <section>
+      <section data-aos="fade-up">
         <h2 className="text-2xl font-bold px-12">Makeup Artist</h2>
         <ScrollableCards type="mua" data={cardData.mua} />
       </section>
 
-      <section>
+      <section data-aos="fade-up">
         <Testimonial />
       </section>
 
-      <section>
+      <section data-aos="fade-up">
         <Contact />
       </section>
     </>
